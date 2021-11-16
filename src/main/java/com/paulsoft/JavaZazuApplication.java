0200970806078cbc5a903bc1c9a1ea16c689e714
Paulo Henrique Capuzzo <paulocapucci@gmail.com>
2021-11-16 17:55:33 -0300

    Primeiro

---
+++ src/main/java/com/paulsoft/JavaZazuApplication.java
@@ -0,0 +1,13 @@
+package com.paulsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaZazuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaZazuApplication.class, args);
+	}
+
+}
